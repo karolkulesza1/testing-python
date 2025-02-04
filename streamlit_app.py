4fba9033f78d5f8b32daa178129d572a1fcfbd8e
Karol Kulesza <121136561+karolkulesza1@users.noreply.github.com>
2025-02-04 18:26:30 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Which segment are you?")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Click on the button to get started!"
 )
